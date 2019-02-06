0f46db8adb634061076079294885a6e28078c126
brianloaeza <46685107+brianloaeza@users.noreply.github.com>
2019-02-06 09:03:14 -0800

    Updated Images Project2

---
+++ studio2/script.js
@@ -0,0 +1,11 @@
+// global vaiables
+var noteBook=document.querySelector(#noteBook);
+var marker=document.querySelector(#marker);
+var pen=document.querySelector(#pen);
+var open=false; //boolean value for whether or not the notebook is open
+var aqua=true; //boolean values for marker color
+var crimson=false;
+var addText=0; //clicking pen adds additional writing
+var page=1; //dictates which page you are on
+var colorAqua=0; //clicking marker adds color
+var colorCrimson=0;
