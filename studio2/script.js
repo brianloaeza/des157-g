73986f1f866948d0319fb2246ec62568af6de7ab
brianloaeza <46685107+brianloaeza@users.noreply.github.com>
2019-02-11 07:59:50 -0800

    Simplified Studio 2

--- studio2/script.js
+++ studio2/script.js
@@ -1,49 +1,43 @@
 // global variables
+"use strict";
+console.log('reading js');
 
-// var aqua=true; //boolean values for marker color
-// var crimson=false;
-var answer;
-var userAnswer;
+
 var page=1; //dictates which page you are on
 var open=false; //boolean value for whether or not the notebook is open
 var noteBook = document.querySelector('#noteBook');
-var question=document.querySelector('.question');
-var submit=document.querySelector('.submit');
 var noteBookOpen1Text=document.querySelector('.noteBookOpen1Text');
 var noteBookOpen1Blue=document.querySelector('.noteBookOpen1Blue');
-var tears=document.querySelector('.tears');
-var me=document.querySelector('.me');
 var marker=document.querySelector('#marker');
 var pen=document.querySelector('#pen');
 
+//hover effects on pen and marker
+marker.addEventListener('mouseover', function() {
+  marker.src='itemsInBackPackMarkerHighlight.png';
+});
+marker.addEventListener('mouseout', function() {
+  marker.src='itemsInBackPackMarker2.png';
+});
+pen.addEventListener('mouseover', function() {
+  pen.src='itemsInBackPackPenHighlight.png';
+});
+pen.addEventListener('mouseout', function() {
+  pen.src='itemsInBackPackPen2.png';
+});
+
 //opens notebook
 noteBook.addEventListener("click", function(start){
-noteBook.src='itemsInBackPackNotebookOpen1.png';
-question.style.display='block';
-submit.style.display='block';
+noteBook.src='noteBookWriting.png';
 open=true;
-
-//used to change pages/answer riddles
-// document.querySelector("form").addEventListener("submit", processForm);
 
 //1st Page
 if(open===true&&page===1){
-var answer='I suffered today';
-pen.addEventListener("click", function(){
-  noteBookOpen1Text.style.display='block';
-  marker.addEventListener("click", function(){
-    noteBookOpen1Blue.style.display='block';
-  });
-});
-}
 
-//2nd Page
-if(open===true&&page===2){
 pen.addEventListener("click", function(){
-  noteBookOpen1Text.style.display='block';
-  marker.addEventListener("click", function(){
-    noteBookOpen1Blue.style.display='block';
-  });
+  noteBook.src='noteBookWritingPen.png';
+});
+marker.addEventListener("click", function(){
+  noteBook.src='notebookCode.png';
 });
 }
 });
