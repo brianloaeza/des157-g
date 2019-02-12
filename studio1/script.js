f1a6f758d75dfc8b83af4f6096c49bdf74e6b8d5
brianloaeza <46685107+brianloaeza@users.noreply.github.com>
2019-02-11 17:41:43 -0800

    opdated studio1

--- studio1/script.js
+++ studio1/script.js
@@ -1,30 +1,18 @@
 "use strict";
 /*capture the submit event with an event listener for onclick and a callback function of processForm
 */
-document.querySelector("form").addEventListener("submit", processForm);
+document.querySelector('#s').addEventListener('click', processForm);
+// document.querySelector('#r').addEventListener('click', resetForm);
 
 //define process function
 function processForm(evt) {
   /*userName variable captures the value from the form input element with the id userName
   */
-  var userName = document.querySelector("#userName").value;
+  var adj = document.querySelector("#adj").value;
+  var noun = document.querySelector("#noun").value;
+  var verb = document.querySelector("#verb").value;
 
-  //give feedback
-  alert("Hi " + userName);
+  console.log(adj.value);
 
-  /*prompt1: declare a variable favMovie to caputure the value from the form input text element with the id favMovie
-  */
-
-  /*prompt2: declare a variable myMsg to capture the html element with the id myMsg
-  */
-
-  /* prompt3: comment out the call to alert() above that says hi to the user
-  */
-
-  /* prompt4: use innerHTML to concatenate a message that includes the userName and the favMovie
-  */
-
-  // prevent page from reloading
   evt.preventDefault();
-  return false;
 }
