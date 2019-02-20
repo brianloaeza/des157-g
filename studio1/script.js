8ea9e6186465d0d119277d20d063dde3024f6507
brianloaeza <46685107+brianloaeza@users.noreply.github.com>
2019-02-20 08:17:37 -0800

    added section

--- studio1/script.js
+++ studio1/script.js
@@ -2,17 +2,28 @@
 /*capture the submit event with an event listener for onclick and a callback function of processForm
 */
 document.querySelector('#s').addEventListener('click', processForm);
-// document.querySelector('#r').addEventListener('click', resetForm);
+document.querySelector('#r').addEventListener('click', resetForm);
 
 //define process function
 function processForm(evt) {
   /*userName variable captures the value from the form input element with the id userName
   */
-  var adj = document.querySelector("#adj").value;
-  var noun = document.querySelector("#noun").value;
-  var verb = document.querySelector("#verb").value;
+  var adj = document.querySelector('#adj').value;
+  var noun = document.querySelector('#noun').value;
+  var verb = document.querySelector('#verb').value;
 
-  console.log(adj.value);
+  // myMsg.innerHTML =
+  //   'Hi, ' +
+  //   userName +
+  //   '!<p>You love the color <em>' +
+  //   favColor +
+  //   '</em>!  Have a great day!</p>';
+  //
+  // msgSection.className = 'show';
 
   evt.preventDefault();
+}
+
+function resetForm() {
+  msgSection.className = 'hide';
 }
