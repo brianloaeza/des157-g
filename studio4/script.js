1298ecbebc39cedf0e97c2297fd23cf9b942c7a0
brianloaeza <46685107+brianloaeza@users.noreply.github.com>
2019-02-25 09:33:25 -0800

    fixed error on js

--- studio4/script.js
+++ studio4/script.js
@@ -6,14 +6,15 @@ var img1 = bodymovin.loadAnimation({
   loop: true,
   autoplay: true,
   path: 'img1.json'
+})
 
-  // var img3 = bodymovin.loadAnimation({
-  //   container: document.querySelector('.img4'),
-  //   renderer: 'svg',
-  //   loop: true,
-  //   autoplay: true,
-  //   path: 'img3.json'
-  // })
+  var img3 = bodymovin.loadAnimation({
+    container: document.querySelector('.img4'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'img3.json'
+  })
 var img4 = bodymovin.loadAnimation({
   container: document.getElementById('img4'),
   renderer: 'svg',
