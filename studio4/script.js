5c829aa6a2870fa86cea88f83335ca0435194c6c
brianloaeza <46685107+brianloaeza@users.noreply.github.com>
2019-02-25 09:30:47 -0800

    Update script.js

--- studio4/script.js
+++ studio4/script.js
@@ -7,13 +7,13 @@ var img1 = bodymovin.loadAnimation({
   autoplay: true,
   path: 'img1.json'
 
-  var img3 = bodymovin.loadAnimation({
-    container: document.querySelector('.img4'),
-    renderer: 'svg',
-    loop: true,
-    autoplay: true,
-    path: 'img3.json'
-  })
+  // var img3 = bodymovin.loadAnimation({
+  //   container: document.querySelector('.img4'),
+  //   renderer: 'svg',
+  //   loop: true,
+  //   autoplay: true,
+  //   path: 'img3.json'
+  // })
 var img4 = bodymovin.loadAnimation({
   container: document.getElementById('img4'),
   renderer: 'svg',
