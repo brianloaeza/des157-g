14de9de543d81b8ed1a1d1c4de224064d0fa9b5d
brianloaeza <46685107+brianloaeza@users.noreply.github.com>
2019-02-27 08:56:17 -0800

    added Visual Research

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 var word=false;//boolean used for text visibility manipulation
 var twinkle=30;//sets changing element of star
 var flap=1;//constant used to pulsate the star
-
-
+// Rain rain;
+// Rain[] drops= new Rain[3000];
+// let drops = [];
 
 function setup() {
   var myCanvas = createCanvas(860, 250);
+  // for(int i=0; i<200; i++){
+  // drops[i]= new Rain(int(random(860)));
+  // rain= new Rain(int(random(860)));
+  // }
 
   myCanvas.parent('mySketch');
 }
@@ -23,6 +28,12 @@ function draw() {
   ellipse(mouseX, mouseY, twinkle+10, 10);
   ellipse(mouseX, mouseY, 10, twinkle+10);
   twinkle+=flap;
+
+  //makes rain
+  // for(int i=0; i<200; i++){
+  // drops[i].display();
+  // drops[i].fall();
+  // }
 
   //sets the conditions of the twinkle
   if (twinkle>=30) {
@@ -47,3 +58,30 @@ function mouseClicked() {
     word=false;
   }
 }
+
+//makes rain
+// class Rain{
+// constructor(){
+ // this.x;//x coordinate of rain
+//  this.fall=3;
+//  this.y=random(0,250);
+// }
+
+ //constructor
+//  Rain(var xPos){
+//  this.x=xPos;
+//  }
+//
+//  display(){
+//   noStroke();
+//   fill(245, 245, 220);
+//   rect(this.x,this.y,2,10);
+//  }
+//
+//   fall(){
+//   this.y+=fall;
+//     if(this.y>250){
+//       this.y=0;
+//   }
+// }
+// }
