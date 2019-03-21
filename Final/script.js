4997fb8a6e6e2e1e4ad839ec8ae1d6aa03399716
brianloaeza <46685107+brianloaeza@users.noreply.github.com>
2019-03-21 16:52:24 -0700

    added to Final

--- Final/script.js
+++ Final/script.js
@@ -7,33 +7,51 @@ var other = document.querySelector('#ot');
 var yesNo = document.querySelector('.yesNo');
 var yes = document.querySelector('#y');
 var no = document.querySelector('#n');
+var sure = document.querySelector('.sure');
+var su = document.querySelector('#su');
 var submission = document.querySelector('.submission');
-var age = document.querySelector('.age');
-var gender = document.querySelector('.gender');
-var status = document.querySelector('.status');
+var enter = document.querySelector('#e');
+var paper = document.querySelector('.paper');
 var leave = document.querySelector('.leave');
 var enter=false;
 
-var img1 = bodymovin.loadAnimation({
+var anim;
+var img1 = {
   container: document.querySelector('.game'),
   renderer: 'svg',
-  loop: true,
+  loop: false,
   autoplay: true,
+  autoloadSegments: false,
   path: 'data.json'
+}
+anim = bodymovin.loadAnimation(img1);
+anim.addEventListener('DOMLoaded', function(){
+anim.playSegments([0,80],true);
 })
 
 english.addEventListener("click", function(){
   language.style.display='none';
   yesNo.style.display='block';
+  anim.playSegments([120,180],true);
+
   yes.addEventListener("click", function(){
+    anim.playSegments([181,270],true);
     yesNo.style.display='none';
-    gender.style.display='block';
-    age.style.display='block';
-    status.style.display='block';
-    submission.style.display='block';
+    sure.style.display='block';
+
+    sure.addEventListener("click", function(){
+      // anim.playSegments([181,270],true);
+      sure.style.display='none';
+      paper.style.display='block';
+      submission.style.display='block';
+
+      enter.addEventListener("click", function(){
+        anim.playSegments([390,510],true);
+        paper.style.display='none';
+        submission.style.display='none';
+      })
+    })
   })
-
-
 })
 
 spanish.addEventListener("click", function(){
@@ -48,11 +66,11 @@ spanish.addEventListener("click", function(){
   })
 })
 
-leave.addEventListener("click", function(){
-  // yesNo.style.display='none';
-  // gender.style.display='none';
-  // age.style.display='none';
-  // status.style.display='none';
-  submission.style.display='none';
-  language.style.display='block';
-})
+// leave.addEventListener("click", function(){
+//   // yesNo.style.display='none';
+//   // gender.style.display='none';
+//   // age.style.display='none';
+//   // status.style.display='none';
+//   submission.style.display='none';
+//   language.style.display='block';
+// })
