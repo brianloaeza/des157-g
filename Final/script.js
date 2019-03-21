fe8cf05469b4319e775ffe5480515313e77f1c44
brianloaeza <46685107+brianloaeza@users.noreply.github.com>
2019-03-21 12:35:12 -0700

    Added Stuff

---
+++ Final/script.js
@@ -0,0 +1,58 @@
+"use strict"
+
+var language = document.querySelector('.language');
+var spanish = document.querySelector('#sp');
+var english = document.querySelector('#en');
+var other = document.querySelector('#ot');
+var yesNo = document.querySelector('.yesNo');
+var yes = document.querySelector('#y');
+var no = document.querySelector('#n');
+var submission = document.querySelector('.submission');
+var age = document.querySelector('.age');
+var gender = document.querySelector('.gender');
+var status = document.querySelector('.status');
+var leave = document.querySelector('.leave');
+var enter=false;
+
+var img1 = bodymovin.loadAnimation({
+  container: document.querySelector('.game'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'data.json'
+})
+
+english.addEventListener("click", function(){
+  language.style.display='none';
+  yesNo.style.display='block';
+  yes.addEventListener("click", function(){
+    yesNo.style.display='none';
+    gender.style.display='block';
+    age.style.display='block';
+    status.style.display='block';
+    submission.style.display='block';
+  })
+
+
+})
+
+spanish.addEventListener("click", function(){
+  language.style.display='none';
+  siNo.style.display='block';
+  yes.addEventListener("click", function(){
+    yesNo.style.display='none';
+    gender.style.display='block';
+    age.style.display='block';
+    status.style.display='block';
+    submission.style.display='block';
+  })
+})
+
+leave.addEventListener("click", function(){
+  // yesNo.style.display='none';
+  // gender.style.display='none';
+  // age.style.display='none';
+  // status.style.display='none';
+  submission.style.display='none';
+  language.style.display='block';
+})
