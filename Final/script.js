eacdb253f90a661373cb95593cbbadb5acb7bcaf
brianloaeza <46685107+brianloaeza@users.noreply.github.com>
2019-03-21 17:14:13 -0700

    beutified code

--- Final/script.js
+++ Final/script.js
@@ -13,57 +13,57 @@ var submission = document.querySelector('.submission');
 var enter = document.querySelector('#e');
 var paper = document.querySelector('.paper');
 var leave = document.querySelector('.leave');
-var enter=false;
+var enter = false;
 
 var anim;
 var img1 = {
-  container: document.querySelector('.game'),
-  renderer: 'svg',
-  loop: false,
-  autoplay: true,
-  autoloadSegments: false,
-  path: 'data.json'
+    container: document.querySelector('.game'),
+    renderer: 'svg',
+    loop: false,
+    autoplay: true,
+    autoloadSegments: false,
+    path: 'data.json'
 }
 anim = bodymovin.loadAnimation(img1);
-anim.addEventListener('DOMLoaded', function(){
-anim.playSegments([0,80],true);
+anim.addEventListener('DOMLoaded', function() {
+    anim.playSegments([0, 80], true);
 })
 
-english.addEventListener("click", function(){
-  language.style.display='none';
-  yesNo.style.display='block';
-  anim.playSegments([120,180],true);
+english.addEventListener("click", function() {
+    language.style.display = 'none';
+    yesNo.style.display = 'block';
+    anim.playSegments([120, 180], true);
 
-  yes.addEventListener("click", function(){
-    anim.playSegments([181,270],true);
-    yesNo.style.display='none';
-    sure.style.display='block';
+    yes.addEventListener("click", function() {
+        anim.playSegments([181, 270], true);
+        yesNo.style.display = 'none';
+        sure.style.display = 'block';
 
-    sure.addEventListener("click", function(){
-      // anim.playSegments([181,270],true);
-      sure.style.display='none';
-      paper.style.display='block';
-      submission.style.display='block';
+        sure.addEventListener("click", function() {
+            // anim.playSegments([181,270],true);
+            sure.style.display = 'none';
+            paper.style.display = 'block';
+            submission.style.display = 'block';
 
-      enter.addEventListener("click", function(){
-        anim.playSegments([390,510],true);
-        paper.style.display='none';
-        submission.style.display='none';
-      })
+            enter.addEventListener("click", function() {
+                anim.playSegments([390, 510], true);
+                paper.style.display = 'none';
+                submission.style.display = 'none';
+            })
+        })
     })
-  })
 })
 
-spanish.addEventListener("click", function(){
-  language.style.display='none';
-  siNo.style.display='block';
-  yes.addEventListener("click", function(){
-    yesNo.style.display='none';
-    gender.style.display='block';
-    age.style.display='block';
-    status.style.display='block';
-    submission.style.display='block';
-  })
+spanish.addEventListener("click", function() {
+    language.style.display = 'none';
+    siNo.style.display = 'block';
+    yes.addEventListener("click", function() {
+        yesNo.style.display = 'none';
+        gender.style.display = 'block';
+        age.style.display = 'block';
+        status.style.display = 'block';
+        submission.style.display = 'block';
+    })
 })
 
 // leave.addEventListener("click", function(){
